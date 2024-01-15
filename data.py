9dbdd324e95bd89a47e7d9cb95b5d08680111155
Colten Bayles <cbayles@outlook.com>
2024-01-15 14:22:58 -0500

    V - 2 Set cart title & labeled axes as well as set size of tick labels

--- data.py
+++ data.py
@@ -3,5 +3,16 @@ import matplotlib.pyplot as plt
 squars = [1, 4, 9, 16, 25]
 fix, ax = plt.subplots() #Common matplotlib convention - calling the subplots() function. This function can generate oone or more plots in the same figure. 
 # The variable 'fig' represents the entire figure or collection of plots that will be generated. The variable 'ax' represents a single plot in the figure and is the variable we'll use most of the time.
+ax.plot(squars, linewidth=3)
+
+# Set chart title and label axes.
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+#Set size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
+
 plt.show() #using the plot method to attempt to plot data. 
 
