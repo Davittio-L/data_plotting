e9e20b11bd7d89a7c9e03f203a7c6e2880b5b81d
Colten Bayles <cbayles@outlook.com>
2024-01-15 14:29:56 -0500

    V 4 added seaborn style and tested scatter

--- data.py
+++ data.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+plt.style.use('seaborn')
 fix, ax = plt.subplots() #Common matplotlib convention - calling the subplots() function. This function can generate oone or more plots in the same figure. 
 # The variable 'fig' represents the entire figure or collection of plots that will be generated. The variable 'ax' represents a single plot in the figure and is the variable we'll use most of the time.
+ax.scatter(2, 4)
 ax.plot(input_values, squares, linewidth=3)
 
 # ax.plot(squares, linewidth=3)
