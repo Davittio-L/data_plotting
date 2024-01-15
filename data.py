d3cfd3cba2fb2585ef1c7f0975818ab76d89d601
Colten Bayles <cbayles@outlook.com>
2024-01-15 14:20:06 -0500

    V 1 - Setup simple line graph

---
+++ data.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+squars = [1, 4, 9, 16, 25]
+fix, ax = plt.subplots() #Common matplotlib convention - calling the subplots() function. This function can generate oone or more plots in the same figure. 
+# The variable 'fig' represents the entire figure or collection of plots that will be generated. The variable 'ax' represents a single plot in the figure and is the variable we'll use most of the time.
+plt.show() #using the plot method to attempt to plot data. 
+
